4b90afb7037a61e726bdbb71dd4d9860c516a3c0
Carlos Quiroga <cquiro@gmail.com>
2017-02-06 14:23:37 -0300

    Generate model Connection and write the self-referential associations with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :comments
   has_many :favorites
   has_many :wishlists
+  has_many :connections
+  has_many :followees, through: :connections
+  has_many :inverse_connections, 
+              class_name: "Connection", foreign_key: "followee_id"
+  has_many :followers, through: :inverse_connections, source: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206163318) do
+ActiveRecord::Schema.define(version: 20170206163829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20170206163318) do
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_comments_on_book_id", using: :btree
     t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
+
+  create_table "connections", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "followee_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "favorites", force: :cascade do |t|
