1a74a206ed1e651409d70f42a79196549d3d7869
Carlos Quiroga <cquiro@gmail.com>
2017-02-08 11:03:30 -0300

    Install devise and add to User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base # :nodoc:
   protect_from_forgery with: :exception
+
+  before_action :configure_permited_parameters, if: :devise_controller?
+
+  protected
+
+  # allow user to sign up with a name
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@
 # several other users. This model is designed to have a self-referential
 # association implemented via a self-join table named connections.
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :comments
   has_many :favorites
   has_many :wishlists
