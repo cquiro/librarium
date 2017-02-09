d58c7de003624dda7480579de93e320da88aa858
Carlos Quiroga <cquiro@gmail.com>
2017-02-09 20:43:25 -0300

    Adding versioning and api subdomain to routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@
 #     user = User.new(user_params)
 #     if user.save
 #       render json: user, status: 201
-      
 #     else
 #       render json: { errors: user.errors }, status: 422
 #     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # resources :users, only: [:create], defaults: { format: 'json' }
+
+  # API definition
+  namespace :api, defaults: { format: :json },
+                  constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1 do
+
+    end
+  end
 end
