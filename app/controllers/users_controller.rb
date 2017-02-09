d0d81a498e2128dc36bf0bb311895dc02e8ef1ed
Carlos Quiroga <cquiro@gmail.com>
2017-02-09 19:28:30 -0300

    Add devise controller helper line to rails_helper.rb file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
-class UsersController < ApplicationController
-end
+# class UsersController < ApplicationController
+#   respond_to :json
+
+#   def create
+#     user = User.new(user_params)
+#     if user.save
+#       render json: user, status: 201
+      
+#     else
+#       render json: { errors: user.errors }, status: 422
+#     end
+#   end
+# end
