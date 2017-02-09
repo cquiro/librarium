a02e3f5b1155824dfec7c4230e04f603ccabef57
Carlos Quiroga <cquiro@gmail.com>
2017-02-09 15:10:46 -0300

    Write first tests for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name "MyString"
-    email "MyString"
+    name Faker::Name.name 
+    email Faker::Internet.unique.email
+    password "123456"
+    password_confirmation "123456"
   end
 end
